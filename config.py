173c2c9a111a94282dd0fc83ae22076332aa23ff
Abdul Razak <sendmesymbols@gmail.com>
2020-08-25 23:06:16 +0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+TOP_LEVEL_DIR = os.path.abspath(os.curdir)
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'add-your-random-key-here'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://localhost/flaskappblueprint'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:yaKhudaKhair@localhost/app'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BCRYPT_LOG_ROUNDS = 15
+    '''
+    MAIL_SERVER = 'smtp.mandrillapp.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'sendmesymbols'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'P@55Word'
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or 'sendmesymbols@gmail.com'
+    '''
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'sendmesymbols@gmail.com'
+    MAIL_PASSWORD = 'snqrlcaymrzzeahm'
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or 'sendmesymbols@gmail.com'
+
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
